3e8371293d397f40323daae2c68c28a44cc5d94b
Latios Thinh <nguyenquocthinh91@gmail.com>
2017-06-28 18:33:32 +0800

    edit feedback

--- kyber/js/index.js
+++ kyber/js/index.js
@@ -1,12 +1,3 @@
-wow = new WOW ({
-  boxClass:     'wow',      // default
-  animateClass: 'animated', // default
-  offset:       0,          // default
-  mobile:       true,       // default
-  live:         true        // default
-})
-wow.init();
-
 smoothScroll.init();
 
 function update() {
