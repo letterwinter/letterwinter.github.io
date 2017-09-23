c97b9b702d0377b013711b7c067d50b8981cdf9a
Latios Thinh <nguyenquocthinh91@gmail.com>
2017-09-23 12:53:28 +0800

    sorry

---
+++ t/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  // $(".g1").hover(
+  //   function () {
+  //     $(this).find(".rect1").removeClass('rect1-out').addClass('rect1-in');
+  //   },
+  //   function () {
+  //     $(this).find(".rect1").addClass('rect1-out').removeClass('rect1-in');
+  //   }
+  // );
+
+});
